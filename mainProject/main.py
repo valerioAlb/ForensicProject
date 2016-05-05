63b658c23750b06c015957d76c30bc36d159ad49
valerioAlb <valerio.albini01@gmail.com>
2016-05-05 14:53:46 +0200

    Fix main.py

--- mainProject/main.py
+++ mainProject/main.py
@@ -5,8 +5,6 @@ import os
 
 LOG_PATH = './LOG.log'
 
-#print methods.WalkPath('/home/valerio/Documenti/Forensic/TEST FILE METADATA')
-
 if __name__ == '__main__':
 
     logging.basicConfig(filename='./LOG.log',level=logging.DEBUG,format=' [%(levelname)s] %(asctime)s %(message)s')
@@ -15,10 +13,12 @@ if __name__ == '__main__':
     parser.printMimeSupported()
 
     if os.path.isfile(LOG_PATH):
+        breackpointFile = '###'
         file = open(LOG_PATH, 'rb')
     else:
         breackpointFile = '###'
 
     methodClass = walkerClass.Walker(parser,breackpointFile)
     methodClass.WalkPath('/home/valerio/Documenti/Forensic/TEST FILE METADATA')
+
     logging.info('Program Ended')
